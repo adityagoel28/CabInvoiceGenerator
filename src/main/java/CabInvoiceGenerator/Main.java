aa6958722fa3405b0b9bb2782f4d8488ae3f23cd
adityagoel28 <adityagoel2002@gmail.com>
2023-12-29 14:38:44 +0530

    Calculated Fare

--- src/main/java/CabInvoiceGenerator/Main.java
+++ src/main/java/CabInvoiceGenerator/Main.java
@@ -1,7 +1,22 @@
 package CabInvoiceGenerator;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Cab Invoice Generator program!");
+
+        CabInvoiceGenerator generator = new CabInvoiceGenerator();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter distance in kilometers:");
+        double distance = sc.nextDouble();
+
+        System.out.println("Enter time in minutes:");
+        double time = sc.nextDouble();
+
+        // Fare calculation
+        double fare = generator.calculateFare(distance, time);
+        System.out.println("Total Fare: Rs. " + fare);
     }
 }
