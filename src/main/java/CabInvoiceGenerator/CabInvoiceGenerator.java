2b917f09635c1e82a894722f35e727bdd7138b29
adityagoel28 <adityagoel2002@gmail.com>
2023-12-29 23:18:00 +0530

    Added option for Premium Rides also

--- src/main/java/CabInvoiceGenerator/CabInvoiceGenerator.java
+++ src/main/java/CabInvoiceGenerator/CabInvoiceGenerator.java
@@ -3,25 +3,48 @@ package CabInvoiceGenerator;
 import java.util.List;
 
 public class CabInvoiceGenerator {
+    // Enum for Ride Types
+    public enum RideType {
+        NORMAL(10.0, 1.0, 5.0),
+        PREMIUM(15.0, 2.0, 20.0);
 
-    private static final double COST_PER_KILOMETER = 10.0;
-    private static final double COST_PER_MINUTE = 1.0;
-    private static final double MINIMUM_FARE = 5.0;
+        private final double costPerKilometer;
+        private final double costPerMinute;
+        private final double minimumFare;
 
-    public double calculateFare(double distanceInKilometers, double timeInMinutes) {
-        double totalFare = (distanceInKilometers * COST_PER_KILOMETER) + (timeInMinutes * COST_PER_MINUTE);
-        return Math.max(totalFare, MINIMUM_FARE);
+        RideType(double costPerKilometer, double costPerMinute, double minimumFare) {
+            this.costPerKilometer = costPerKilometer;
+            this.costPerMinute = costPerMinute;
+            this.minimumFare = minimumFare;
+        }
+
+        public double getCostPerKilometer() {
+            return costPerKilometer;
+        }
+
+        public double getCostPerMinute() {
+            return costPerMinute;
+        }
+
+        public double getMinimumFare() {
+            return minimumFare;
+        }
+    }
+
+    public double calculateFare(double distanceInKilometers, double timeInMinutes, RideType rideType) {
+        double totalFare = (distanceInKilometers * rideType.getCostPerKilometer()) + (timeInMinutes * rideType.getCostPerMinute());
+        return Math.max(totalFare, rideType.getMinimumFare());
     }
 
     public double calculateTotalFareForMultipleRides(List<Ride> rides) {
         return rides.stream()
-                .mapToDouble(ride -> calculateFare(ride.getDistance(), ride.getTime()))
+                .mapToDouble(ride -> calculateFare(ride.getDistance(), ride.getTime() , ride.getRideType()))
                 .sum();
     }
 
     public InvoiceSummary calculateInvoiceSummary(List<Ride> rides) {
         double totalFare = rides.stream()
-                .mapToDouble(ride -> calculateFare(ride.getDistance(), ride.getTime()))
+                .mapToDouble(ride -> calculateFare(ride.getDistance(), ride.getTime(), ride.getRideType()))
                 .sum();
         return new InvoiceSummary(rides.size(), totalFare);
     }
