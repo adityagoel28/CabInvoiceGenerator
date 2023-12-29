38633f5aad84e9f420740bf8a5f7b87810c5f7ad
adityagoel28 <adityagoel2002@gmail.com>
2023-12-29 23:05:45 +0530

    Invoice Service

--- src/main/java/CabInvoiceGenerator/Main.java
+++ src/main/java/CabInvoiceGenerator/Main.java
@@ -9,26 +9,47 @@ public class Main {
         System.out.println("Cab Invoice Generator program!");
 
         Scanner sc = new Scanner(System.in);
-        CabInvoiceGenerator generator = new CabInvoiceGenerator();
-        List<Ride> rides = new ArrayList<>();
+//        CabInvoiceGenerator generator = new CabInvoiceGenerator();
+//        List<Ride> rides = new ArrayList<>();
+//
+//        System.out.println("Enter the number of rides:");
+//        int numRides = sc.nextInt();
+//
+//        for(int i = 0; i < numRides; i++){
+//            System.out.println("Enter distance for ride " + (i + 1) + " in kilometers:");
+//            double distance = sc.nextDouble();
+//
+//            System.out.println("Enter time for ride " + (i + 1) + " in minutes:");
+//            double time = sc.nextDouble();
+//
+//            rides.add(new Ride(distance, time));
+//        }
+//
+//        double totalFare = generator.calculateTotalFareForMultipleRides(rides);
+//        System.out.println("Total Fare for all rides: Rs. " + totalFare);
 
-        System.out.println("Enter the number of rides:");
-        int numRides = sc.nextInt();
+//        InvoiceSummary summary = generator.calculateInvoiceSummary(rides);
+//        System.out.println(summary);
 
-        for(int i = 0; i < numRides; i++){
-            System.out.println("Enter distance for ride " + (i + 1) + " in kilometers:");
-            double distance = sc.nextDouble();
+        RideRepository rideRepository = new RideRepository();
+        CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
+        InvoiceService invoiceService = new InvoiceService(rideRepository, invoiceGenerator);
 
-            System.out.println("Enter time for ride " + (i + 1) + " in minutes:");
-            double time = sc.nextDouble();
+        // Adding some test rides to the repository for different users
+        rideRepository.addRide("user1", new Ride(2.0, 5));
+        rideRepository.addRide("user1", new Ride(3.0, 10));
+        rideRepository.addRide("user2", new Ride(5.0, 15));
+        rideRepository.addRide("user2", new Ride(0.5, 3));
 
-            rides.add(new Ride(distance, time));
+        System.out.println("Enter User ID to generate invoice:");
+        String userId = sc.next();
+
+        // Generating and displaying the invoice for the requested user
+        try {
+            InvoiceSummary summary = invoiceService.getInvoiceForUser(userId);
+            System.out.println("Invoice for user " + userId + ": " + summary);
+        } catch (Exception e) {
+            System.out.println("Error generating invoice: " + e.getMessage());
         }
-
-        double totalFare = generator.calculateTotalFareForMultipleRides(rides);
-        System.out.println("Total Fare for all rides: Rs. " + totalFare);
-
-        InvoiceSummary summary = generator.calculateInvoiceSummary(rides);
-        System.out.println(summary);
     }
 }
